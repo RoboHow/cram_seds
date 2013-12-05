4990df8dbdff80e739f2a45e0283cd08226b7362
Moritz Tenorth <tenorth@cs.uni-bremen.de>
2013-12-05 16:40:36 +0100

    updated test client

--- knowrob_seds/src/test_seds_model.py
+++ knowrob_seds/src/test_seds_model.py
@@ -9,16 +9,21 @@ if __name__ == '__main__':
     rospy.init_node('test_json_prolog')
     prolog = json_prolog.Prolog()
     query = prolog.query("""rdfs_subclass_of(Phase, seds:'SEDSMotion'),
-                            motion_gmms(Phase, GMM), 
-                            gmm_gaussians(GMM, Gaussian), 
-                            gaussian_components(Gaussian, Mean, Cov, Prior), 
-                            vector_elements(Mean, MeanVec), 
+                            phase_properties(Phase, Models),
+                            member(Model, Models),
+                            motion_properties(Model, Type, GMMs),
+                            member(GMM, GMMs),
+                            gmm_properties(GMM, InputType, InputDim, OutputType, OutputDim, Gaussians),
+                            member(Gaussian, Gaussians),
+                            gaussian_components(Gaussian, Mean, Cov, Prior),
+                            vector_elements(Mean, MeanVec),
                             matrix_elements(Cov, CovMat)""")
-                            
-    
+
+
     for solution in query.solutions():
         print "\n\n= = = = = = = = = = = = = = = = = = = = = "
         print solution['Phase'].split('#')[1]
+        print solution['Model'].split('#')[1]
         print solution['GMM'].split('#')[1]
         print solution['Gaussian'].split('#')[1]
         print 'Mean = %s' % (solution['MeanVec'])
