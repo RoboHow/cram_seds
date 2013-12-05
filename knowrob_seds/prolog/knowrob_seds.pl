de389a24da73c13c9ee40c496999e7df58882ffc
Moritz Tenorth <tenorth@cs.uni-bremen.de>
2013-12-05 16:30:44 +0100

    adapted query predicates to new task structure

--- knowrob_seds/prolog/knowrob_seds.pl
+++ knowrob_seds/prolog/knowrob_seds.pl
@@ -21,15 +21,12 @@
 
 :- module(knowrob_seds,
     [
+      phase_properties/2,
+      motion_properties/3,
+      gmm_properties/6,
+      gaussian_components/4,
       vector_elements/2,
-      matrix_elements/2,
-      motion_gmms/2,
-      motion_master_gmm/2,
-      motion_slave_gmm/2,
-      motion_force_gmm/2,
-      motion_coupling_gmm/2,
-      gmm_gaussians/2,
-      gaussian_components/4
+      matrix_elements/2
     ]).
 
 :- use_module(library('semweb/rdfs')).
@@ -39,15 +36,12 @@
 :- use_module(library('jpl')).
 
 :-  rdf_meta
+      phase_properties(r,r),
+      motion_properties(r,r,?),
+      gmm_properties(r,?,?,?,?,?),
+      gaussian_components(r,r,r,r),
       vector_elements(r, ?),
-      matrix_elements(r, ?),
-      motion_gmms(r,r),
-      motion_master_gmm(r,r),
-      motion_slave_gmm(r,r),
-      motion_force_gmm(r,r),
-      motion_coupling_gmm(r,r),
-      gmm_gaussians(r,r),
-      gaussian_components(r,r,r,r).
+      matrix_elements(r, ?).
 
 
 :- rdf_db:rdf_register_ns(rdf, 'http://www.w3.org/1999/02/22-rdf-syntax-ns#', [keep(true)]).
@@ -58,32 +52,84 @@
 :- rdf_db:rdf_register_ns(seds, 'http://ias.cs.tum.edu/kb/knowrob-seds.owl#', [keep(true)]).
 
 
-motion_gmms(Motion, GMM) :-
-  class_properties(Motion, P, GMM),
-  rdfs_subproperty_of(P, seds:describedByGMM).
+%% phase_properties(MotionPhase, MotionModels)
+%
+% Read properties of a MotionPhase, in particular the list
+% of models describing it
+%
+% @param MotionPhase   Instance of a seds:'SEDSMotion'
+% @param MotionModels  List of instances of seds:'MotionModel'
+%
+phase_properties(MotionPhase, MotionModels) :-
+  findall(MotionModel,
+      class_properties(MotionPhase, seds:describedByMotionModel, MotionModel),
+      MotionModels).
 
-motion_master_gmm(Motion, GMM) :-
-  class_properties(Motion, P, GMM),
-  rdfs_subproperty_of(P, seds:masterGMM).
-motion_slave_gmm(Motion, GMM) :-
-  class_properties(Motion, P, GMM),
-  rdfs_subproperty_of(P, seds:slaveGMM).
-motion_force_gmm(Motion, GMM) :-
-  class_properties(Motion, P, GMM),
-  rdfs_subproperty_of(P, seds:forceGMM).
-motion_coupling_gmm(Motion, GMM) :-
-  class_properties(Motion, P, GMM),
-  rdfs_subproperty_of(P, seds:couplingGMM).
 
-gmm_gaussians(GMM, Gaussian) :-
-  rdf_has(GMM, seds:gaussianDist, Gaussian).
 
+%% motion_properties(MotionModel, Type, GMMs) is nondet.
+%
+% Read properties of a MotionModel, in particular its type and the
+% GaussianMixtureModels describing it
+%
+% @param MotionModel  Instance of a motion model
+% @param Type         Class of the motion model, subclass of seds:'MotionModel'
+% @param GMMs         List of instances of seds:'GaussianMixtureModel'
+%
+motion_properties(MotionModel, Type, GMMs) :-
+  rdf_has(MotionModel, rdf:type, Type),
+  owl_subclass_of(Type, seds:'MotionModel'),
+  findall(GMM, (rdf_has(MotionModel, seds:describedByGMM, GMM)), GMMs).
+
+
+
+
+%% gmm_properties(GMM, InputType, InputDim, OutputType, OutputDim, Gaussians) is nondet.
+%
+% Read properties of the GaussianMixtureModel GMM
+%
+% @param GMM          Instance of a seds:'GaussianMixtureModel'
+% @param InputType    String describing the quantity of the input values (e.g. Position, Velocity, Other)
+% @param InputDim     String describing which dimensions are described by the input values (e.g. xz, **)
+% @param OutputType   String describing the quantity of the output values (e.g. Position, Velocity, Other)
+% @param OutputDim    String describing which dimensions are described by the output values (e.g. xz, **)
+% @param Gaussians    List of instances of seds:'GaussianDistribution'
+%
+gmm_properties(GMM, InputType, InputDim, OutputType, OutputDim, Gaussians) :-
+  rdf_has(GMM, seds:inputType,  literal(type('http://www.w3.org/2001/XMLSchema#string', InputType))),
+  rdf_has(GMM, seds:inputDim,   literal(type('http://www.w3.org/2001/XMLSchema#string', InputDim))),
+  rdf_has(GMM, seds:outputType, literal(type('http://www.w3.org/2001/XMLSchema#string', OutputType))),
+  rdf_has(GMM, seds:outputDim,  literal(type('http://www.w3.org/2001/XMLSchema#string', OutputDim))),
+  findall(G, rdf_has(GMM, seds:gaussianDist, G), Gaussians).
+
+
+
+
+%% gaussian_components(+Gaussian, -Mean, -Cov, -Prior) is nondet.
+%
+% Read components of a Gaussian distribution
+%
+% @param Gaussian  Instance of seds:GaussianDistribution
+% @param Mean      Mean vector (Instance of knowrob:Vector)
+% @param Cov       Covariance matrix (Instance of knowrob:Matrix)
+% @param Prior     Prior (double value denoting the prior for this Gaussian distribution in a GMM)
+%
 gaussian_components(Gaussian, Mean, Cov, Prior) :-
   rdf_has(Gaussian, seds:mean, Mean),
   rdf_has(Gaussian, seds:cov,  Cov),
   rdf_has(Gaussian, seds:prior, literal(type(xsd:double,P))),
   term_to_atom(Prior, P).
 
+
+
+%% matrix_elements(+Mat, -ElemList) is nondet.
+%
+% Return all elements of the matrix Mat in list ElemList (row-based)
+%
+% @param Mat       Instance of knowrob:Matrix
+% @param ElemList  Row-based list of elements of Mat
+%
+%
 matrix_elements(Mat, ElemList) :-
 
   findall(P-V, (rdfs_subproperty_of(Prop, knowrob:matrixElement),
@@ -95,6 +141,14 @@ matrix_elements(Mat, ElemList) :-
   pairs_values(PVsorted, ElemList).
 
 
+
+%% vector_elements(+Vec, -ElemList) is nondet.
+%
+% Return all elements of the vector Vec in list ElemList
+%
+% @param Vec       Instance of knowrob:Vector
+% @param ElemList  Row-based list of elements of Vec
+%
 vector_elements(Vec, ElemList) :-
 
   findall(P-V, (rdfs_subproperty_of(Prop, knowrob:vectorElement),
