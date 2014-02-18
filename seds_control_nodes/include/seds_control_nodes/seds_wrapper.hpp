89025472e4aa58c4cddc16755f076ab44bea3bee
Georg Bartels <georg.bartels@cs.uni-bremen.de>
2014-02-18 11:45:05 +0100

    Some slight code-reordering plus outline of vel-ik code.

--- seds_control_nodes/include/seds_control_nodes/seds_wrapper.hpp
+++ seds_control_nodes/include/seds_control_nodes/seds_wrapper.hpp
@@ -9,12 +9,15 @@ class SedsWrapper {
 private:
 
     KDL::ChainFkSolverPos_recursive *fk_solver_;
+    unsigned int dof_;
 
+    // memory for output values
+    //KDL::JntArray& q_tmp_;
 public:
-    void parse_urdf(const std::string &param_name, const std::string &root_name, const std::string &tip_name);
-    void get_fk(void);
-
+    void parse_urdf(const std::string &param_name, const std::string &root_name,
+        const std::string &tip_name);
+    KDL::Frame get_fk(const KDL::JntArray& q);
+    //KDL::JntArray& get_vel_ik(const KDL::JntArray& q, const KDL::Frame& des_pose);
 };
-
 
 #endif  // __SEDS_WRAPPER_HPP
--- seds_control_nodes/src/seds_wrapper.cpp
+++ seds_control_nodes/src/seds_wrapper.cpp
@@ -1,65 +1,61 @@
-
 #include "include/seds_control_nodes/seds_wrapper.hpp"
-
 #include <iostream>
 #include <kdl_parser/kdl_parser.hpp>
-
 #include <kdl/chain.hpp>
-
 #include <ros/ros.h>
 
 using namespace std;
 
-void SedsWrapper::parse_urdf(const string &param_name, const string &root_name, const string &tip_name) {
-    KDL::Tree tree;
-    KDL::Chain chain;
+void SedsWrapper::parse_urdf(const string &param_name, const string &root_name,
+    const string &tip_name) 
+{
+  KDL::Tree tree;
+  KDL::Chain chain;
 
-    if ( !kdl_parser::treeFromParam(param_name, tree)) {
-        ROS_ERROR("Could not parse the parameter into a kdl_tree.");
-        throw;
-    }
+  if (!kdl_parser::treeFromParam(param_name, tree)) {
+    ROS_ERROR("Could not parse the parameter into a kdl_tree.");
+    throw;
+  }
 
-    if (!tree.getChain(root_name, tip_name, chain)) {
-        ROS_ERROR("Could not initialize chain object");
-        throw;
-      }
+  if (!tree.getChain(root_name, tip_name, chain)) {
+    ROS_ERROR("Could not initialize chain object");
+    throw;
+  }
 
-    cout << "Nr of Joints: " << chain.getNrOfJoints();
+  dof_ = chain.getNrOfJoints();
 
-    fk_solver_ = new KDL::ChainFkSolverPos_recursive(chain);
-
+  fk_solver_ = new KDL::ChainFkSolverPos_recursive(chain);
+//  q_tmp_.resize(dof_);
 }
 
+KDL::Frame SedsWrapper::get_fk(const KDL::JntArray& q)
+{
+  assert(q.rows() == dof_);
 
+  KDL::Frame pose;
+  fk_solver_->JntToCart(q, pose);
 
-void SedsWrapper::get_fk(void){
-
-    KDL::JntArray jntarray(7);
-
-    for (unsigned int i=0; i < 7; i++)
-        jntarray(i) = 0.0;
-
-    KDL::Frame pose;
-    fk_solver_->JntToCart(jntarray, pose);
-
-    cout << "Position: " << pose.p(0) << "," << pose.p(1) << "," << pose.p(2) << endl;
+  return pose;
 }
 
+int main(int argc, char **argv)
+{
 
-int main(int argc, char **argv) {
+  std::cout << "lalala\n";
 
-    std::cout << "lalala\n";
+  ros::init(argc, argv, "seds_wrapper");
+  //ros::NodeHandle n;
 
-    ros::init(argc, argv, "seds_wrapper");
-    //ros::NodeHandle n;
+  SedsWrapper sedswrapper;
 
-    SedsWrapper sedswrapper;
+  //Get better frames into the URDF
+  sedswrapper.parse_urdf("robot_description", "base_link", "left_arm_7_link");
 
-    //Get better frames into the URDF
-    sedswrapper.parse_urdf("robot_description", "base_link", "left_arm_7_link");
-    sedswrapper.get_fk();
+  KDL::JntArray jntarray(7);
+  for(unsigned int i=0; i < 7; i++)
+    jntarray(i) = 0.0;
+  KDL::Frame pose = sedswrapper.get_fk(jntarray);
+  cout << "Position: " << pose.p(0) << "," << pose.p(1) << "," << pose.p(2) << endl;
 
-    return 0;
-
-
+  return 0;
 };
