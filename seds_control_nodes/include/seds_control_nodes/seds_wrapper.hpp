04b4580b07cd22de39a9e0990ef1e425e915f311
Georg Bartels <georg.bartels@cs.uni-bremen.de>
2014-02-18 13:38:09 +0100

    Added IK-solver, renamed class, and switched to reading Chain from URDF.

--- seds_control_nodes/include/seds_control_nodes/seds_wrapper.hpp
+++ seds_control_nodes/include/seds_control_nodes/seds_wrapper.hpp
@@ -1,23 +1,41 @@
-
-#ifndef __SEDS_WRAPPER_HPP
-#define __SEDS_WRAPPER_HPP
+#ifndef __ARM_KINEMATICS_HPP
+#define __ARM_KINEMATICS_HPP
 
 #include <string>
 #include <kdl/chainfksolverpos_recursive.hpp>
+#include <kdl/chainiksolvervel_wdls.hpp>
+#include <urdf_model/model.h>
 
-class SedsWrapper {
-private:
-
+class ArmKinematics {
+  private:
+    // forward kinematics solver
     KDL::ChainFkSolverPos_recursive *fk_solver_;
+    // velocity-resolved inverse kinematics solver
+    KDL::ChainIkSolverVel_wdls *ik_solver_;
+    // keeping track of the number of joints we have
     unsigned int dof_;
 
-    // memory for output values
-    //KDL::JntArray& q_tmp_;
-public:
-    void parse_urdf(const std::string &param_name, const std::string &root_name,
-        const std::string &tip_name);
-    KDL::Frame get_fk(const KDL::JntArray& q);
-    //KDL::JntArray& get_vel_ik(const KDL::JntArray& q, const KDL::Frame& des_pose);
-};
+    // memory for temporary values
+    KDL::JntArray q_tmp_;
 
-#endif  // __SEDS_WRAPPER_HPP
+    // some internal helper methods
+    void digestIkErrorCode(int error_code) const;
+    KDL::Chain readChainFromUrdf(const urdf::ModelInterface& robot_model,
+        const std::string &root_name, const std::string &tip_name);
+    void initInternals(const KDL::Chain& chain);
+
+  public:
+    ArmKinematics();
+    ArmKinematics(const urdf::ModelInterface& robot_model,
+        const std::string &root_name, const std::string &tip_name);
+    
+    ~ArmKinematics();
+
+    void init(const urdf::ModelInterface& robot_model,
+        const std::string &root_name, const std::string &tip_name);
+
+    KDL::Frame get_fk(const KDL::JntArray& q);
+    KDL::JntArray& get_vel_ik(const KDL::JntArray& q, const KDL::Frame& des_pose,
+        double dt);
+};
+#endif  // __ARM_KINEMATICS_HPP
--- seds_control_nodes/src/seds_wrapper.cpp
+++ seds_control_nodes/src/seds_wrapper.cpp
@@ -5,37 +5,96 @@
 #include <ros/ros.h>
 
 using namespace std;
+using namespace KDL;
 
-void SedsWrapper::parse_urdf(const string &param_name, const string &root_name,
-    const string &tip_name) 
+ArmKinematics::ArmKinematics() :
+    fk_solver_(NULL), ik_solver_(NULL), dof_(0), q_tmp_(JntArray(dof_))
 {
-  KDL::Tree tree;
-  KDL::Chain chain;
-
-  if (!kdl_parser::treeFromParam(param_name, tree)) {
-    ROS_ERROR("Could not parse the parameter into a kdl_tree.");
-    throw;
-  }
-
-  if (!tree.getChain(root_name, tip_name, chain)) {
-    ROS_ERROR("Could not initialize chain object");
-    throw;
-  }
-
-  dof_ = chain.getNrOfJoints();
-
-  fk_solver_ = new KDL::ChainFkSolverPos_recursive(chain);
-//  q_tmp_.resize(dof_);
 }
 
-KDL::Frame SedsWrapper::get_fk(const KDL::JntArray& q)
+ArmKinematics::ArmKinematics(const urdf::ModelInterface& robot_model, 
+    const std::string &root_name, const std::string &tip_name)
+{
+  this->init(robot_model, root_name, tip_name);
+}
+
+ArmKinematics::~ArmKinematics()
+{
+  if(fk_solver_)
+    delete(fk_solver_);
+
+  if(ik_solver_)
+    delete(ik_solver_);
+}
+ 
+void ArmKinematics::digestIkErrorCode(int error_code) const
+{
+  if(KDL::SolverI::E_NOERROR != error_code)
+  {
+    cout << "IK-Solver returned with an error.\n";
+    throw;
+  }
+}
+
+KDL::Frame ArmKinematics::get_fk(const KDL::JntArray& q)
 {
   assert(q.rows() == dof_);
+  assert(fk_solver_);
 
   KDL::Frame pose;
   fk_solver_->JntToCart(q, pose);
 
   return pose;
+}
+
+KDL::JntArray& ArmKinematics::get_vel_ik(const KDL::JntArray& q,
+    const KDL::Frame& des_pose, double dt)
+{
+  assert(ik_solver_);
+  assert(q.rows() == dof_);
+  assert(q.rows() == q_tmp_.rows());
+  // TODO(Georg): set JS & TS weights
+  // TODO(Georg): set lambda
+
+  digestIkErrorCode(
+      ik_solver_->CartToJnt(q, KDL::diff(get_fk(q), des_pose, dt), q_tmp_));
+
+  return q_tmp_;
+}
+
+void ArmKinematics::initInternals(const KDL::Chain& chain)
+{
+  dof_ = chain.getNrOfJoints();
+  q_tmp_.resize(dof_);
+
+  fk_solver_ = new KDL::ChainFkSolverPos_recursive(chain);
+  ik_solver_ = new KDL::ChainIkSolverVel_wdls(chain);
+}
+
+void ArmKinematics::init(const urdf::ModelInterface& robot_model,
+    const string &root_name, const string &tip_name) 
+{
+  KDL::Chain chain = readChainFromUrdf(robot_model, root_name, tip_name);
+  initInternals(chain);
+}
+
+KDL::Chain ArmKinematics::readChainFromUrdf(const urdf::ModelInterface& robot_model,
+    const std::string &root_name, const std::string &tip_name)
+{
+  KDL::Tree tree;
+  KDL::Chain chain;
+
+  if (!kdl_parser::treeFromUrdfModel(robot_model, tree)) {
+    cout << "Could not parse robot model into a kdl_tree.\n";
+    throw;
+  }
+
+  if (!tree.getChain(root_name, tip_name, chain)) {
+    cout << "Could not initialize chain object\n";
+    throw;
+  }
+
+  return chain; 
 }
 
 int main(int argc, char **argv)
@@ -46,16 +105,16 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "seds_wrapper");
   //ros::NodeHandle n;
 
-  SedsWrapper sedswrapper;
+  ArmKinematics sedswrapper;
 
-  //Get better frames into the URDF
-  sedswrapper.parse_urdf("robot_description", "base_link", "left_arm_7_link");
-
-  KDL::JntArray jntarray(7);
-  for(unsigned int i=0; i < 7; i++)
-    jntarray(i) = 0.0;
-  KDL::Frame pose = sedswrapper.get_fk(jntarray);
-  cout << "Position: " << pose.p(0) << "," << pose.p(1) << "," << pose.p(2) << endl;
+//  //Get better frames into the URDF
+//  sedswrapper.init("robot_description", "base_link", "left_arm_7_link");
+//
+//  KDL::JntArray jntarray(7);
+//  for(unsigned int i=0; i < 7; i++)
+//    jntarray(i) = 0.0;
+//  KDL::Frame pose = sedswrapper.get_fk(jntarray);
+//  cout << "Position: " << pose.p(0) << "," << pose.p(1) << "," << pose.p(2) << endl;
 
   return 0;
 };
