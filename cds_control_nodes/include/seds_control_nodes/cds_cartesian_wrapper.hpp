5b63b208f044f25caa9aba43a65005819b1ada2c
Georg Bartels <georg.bartels@cs.uni-bremen.de>
2014-03-13 16:34:55 +0100

    Added: Trivial Cartesian Wrapper around CDSExecution.

---
+++ cds_control_nodes/include/seds_control_nodes/cds_cartesian_wrapper.hpp
@@ -0,0 +1,30 @@
+#ifndef __CDS_CARTESIAN_WRAPPER_HPP
+#define __CDS_CARTESIAN_WRAPPER_HPP
+
+#include <CDSExecution.h>
+
+class CDSExecutionParams
+{
+  public:
+    GMRDynamics* master_dyn_;
+    GMRDynamics* slave_dyn_;
+    GMR* coupling_;
+    double alpha_, beta_, lambda_, reachingThreshold_, dt_;
+    KDL::Frame object_frame_, attractor_frame_;
+};
+
+class CdsCartesianWrapper
+{
+  public:
+    CdsCartesianWrapper();
+    CdsCartesianWrapper(CDSExecutionParams params, const KDL::Frame& pose_init);
+
+    ~CdsCartesianWrapper();
+ 
+    void init(CDSExecutionParams params, const KDL::Frame& pose_init);
+    KDL::Frame update(const KDL::Frame& current_pose);
+
+  private:
+    CDSExecution cds_controller_;
+};
+#endif  // __CDS_CARTESIAN_WRAPPER_HPP
---
+++ cds_control_nodes/src/cds_cartesian_wrapper.cpp
@@ -0,0 +1,38 @@
+#include <seds_control_nodes/cds_cartesian_wrapper.hpp>
+#include <seds_control_nodes/kdl_epfl_conversions.hpp>
+
+CdsCartesianWrapper::CdsCartesianWrapper() :
+    cds_controller_( CDSExecution() ) {}
+
+CdsCartesianWrapper::CdsCartesianWrapper(CDSExecutionParams params,
+    const KDL::Frame& pose_init)
+{
+  this->init(params, pose_init);
+}
+
+CdsCartesianWrapper::~CdsCartesianWrapper() {}
+
+void CdsCartesianWrapper::init(CDSExecutionParams params,
+    const KDL::Frame& pose_init)
+{
+  cds_controller_.setObjectFrame(toMathLib(params.object_frame_));
+
+  cds_controller_.setAttractorFrame(toMathLib(params.attractor_frame_));
+
+  cds_controller_.setDT(params.dt_);
+
+  cds_controller_.setCurrentEEPose(toMathLib(pose_init));
+
+  cds_controller_.init(static_cast<GMRDynamics*>(params.master_dyn_),
+      static_cast<GMRDynamics*>(params.slave_dyn_), 
+      static_cast<GMR*>(params.coupling_));
+
+  cds_controller_.setMotionParameters(params.alpha_, 
+      params.beta_, params.lambda_, params.reachingThreshold_); 
+}
+
+KDL::Frame CdsCartesianWrapper::update(const KDL::Frame& current_pose)
+{
+  cds_controller_.setCurrentEEPose(toMathLib(current_pose));
+  return toKDL(cds_controller_.getNextEEPose());
+}
